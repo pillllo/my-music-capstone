176dd82da99f03f69568227e9907bea8745ff19e
pillllo <natalie.s.pilling@gmail.com>
2024-08-22 11:15:48 +0200

    Add component files

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import Example from "./Example";
+
+function App() {
+  return (
+    <div className="">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Example />
+    </div>
+  );
+}
+
+export default App;
